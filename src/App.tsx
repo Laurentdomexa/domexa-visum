304f892da5d6db9382a32f7479697e8a7fd29bfe
Laurentdomexa <kashiapa@gmx.fr>
2026-03-30 16:54:06 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -492,7 +492,6 @@ export default function LandingPage() {
                     className="w-full bg-blue-900 border border-blue-600 rounded-lg px-4 py-3 text-white placeholder-blue-400 focus:outline-none focus:border-blue-300"
                     placeholder="06 XX XX XX XX"
                   />
-                </div>
               </div>
 </div> {/* fin du champ immeubles */}
 
